14f4cc1cf2e2435d095fabd0833074c8b06e526c
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-30 09:18:32 +0600

    Update auto.js

--- TINA/commands/auto.js
+++ TINA/commands/auto.js
@@ -3,7 +3,6 @@ module.exports = {
     name: "auto",
     version: "0.0.2",
     hasPermssion: 0,
-    credits: "Nayan",
     description: "auto video download",
     commandCategory: "user",
     usages: "",
@@ -18,18 +17,18 @@ handleEvent: async function ({ api, event, args }) {
   const body = content.toLowerCase();
   const {alldown} = require("nayan-videos-downloader")
   if (body.startsWith("https://")) {
-  api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
+  api.setMessageReaction("💔", event.messageID, (err) => {}, true);
 const data = await alldown(content);
   console.log(data)
   const {low, high, title} = data.data;
-    api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
+    api.setMessageReaction("💙", event.messageID, (err) => {}, true);
   const video = (await axios.get(high, {
       responseType: "arraybuffer",
     })).data;
     fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
         return api.sendMessage({
-            body: `《TITLE》: ${title}`,
+            body: `[🤍] `,
             attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
         }, event.threadID, event.messageID);
