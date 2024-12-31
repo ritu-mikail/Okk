9820ca6696b861694b6c57c131840475294180d2
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-31 18:23:23 +0600

    Update goiadmin3.js

--- TINA/commands/goiadmin3.js
+++ TINA/commands/goiadmin3.js
@@ -26,5 +26,9 @@ var callback = () => api.sendMessage({body:`${know}`,attachment: fs.createReadSt
 return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
 
 };
-module.exports.run = async function({}) {
+module.exports.run = async function({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) {
                  }
+
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
