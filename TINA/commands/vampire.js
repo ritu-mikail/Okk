fcbf000fa39524f7eefc63178e866ef1dae58b1f
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-10 23:58:08 +0600

    Update vampire.js

--- TINA/commands/vampire.js
+++ TINA/commands/vampire.js
@@ -3,26 +3,26 @@ const axios = require("axios");
 module.exports.config = {
   name: "vampire",
   commandCategory: "image",
-  hasPermssion: 0,
-  credits: "Tanvir143",
-  author: "Tanvir143"
+  permission: 0,
+  credits: "nazrul",
+  author: "nazrul"
 };
 
 module.exports.run = async ({ api, event, args }) => {
   try {
 
     if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
-      return api.sendMessage("𝘱𝘭𝘦𝘢𝘴𝘦 𝘳𝘦𝘱𝘭𝘢𝘺 𝘵𝘩𝘦 𝘪𝘮𝘢𝘨𝘦.", event.threadID, event.messageID);
+      return api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐑𝐞𝐩𝐥𝐚𝐲 𝐓𝐡𝐞 𝐈𝐦𝐚𝐠𝐞", event.threadID, event.messageID);
     }
-api.sendMessage("𝘤𝘰𝘯𝘷𝘦𝘳𝘵𝘪𝘯𝘨 𝘪𝘮𝘢𝘨𝘦 𝘵𝘰 𝘷𝘢𝘮𝘱𝘪𝘳𝘦.", event.threadID, event.messageID);
+api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 𝐁𝐚𝐛𝐲...😘", event.threadID, event.messageID);
 
-    const Tanvir143 = event.messageReply?.attachments[0]?.url;
-
-
-    const tanvir = (`https://kaiz-apis.gleeze.com/api/vampire?imageUrl=${encodeURIComponent(Tanvir143)}`);
+    const nazrul = event.messageReply?.attachments[0]?.url;
 
 
-    const imageStream = await axios.get(tanvir,{
+    const nazrulb = (`https://kaiz-apis.gleeze.com/api/vampire?imageUrl=${encodeURIComponent(nazrul)}`);
+
+
+    const imageStream = await axios.get(nazrulb,{
       responseType: 'stream'
     });
 
@@ -32,7 +32,7 @@ api.sendMessage("𝘤𝘰𝘯𝘷𝘦𝘳𝘵𝘪𝘯𝘨 𝘪𝘮𝘢𝘨𝘦 �
       attachment: imageStream.data
     }, event.threadID, event.messageID);
 
-  } catch (tamim) {
-    api.sendMessage(`Error: ${tamim.message}`, event.threadID, event.messageID);
+  } catch (nazrulc) {
+    api.sendMessage(`Error: ${nazrulc.message}`, event.threadID, event.messageID);
   }
 };
