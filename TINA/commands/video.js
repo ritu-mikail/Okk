b0ae0be56cc4d938653668c5adf581b5a9f6d2cb
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-11 20:59:54 +0600

    Update video.js

--- TINA/commands/video.js
+++ TINA/commands/video.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const axios = require("axios");
 const fs = require("fs");
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  'Content-Type': 'application/json',
-};
 
 module.exports.config = {
   name: "video",
