c5b1e61efc45bee2f533634175ae2f0eb74f31c6
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-17 15:45:52 +0600

    Update hack.js

--- TINA/commands/hack.js
+++ TINA/commands/hack.js
@@ -83,11 +83,11 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
 	  
 	  
 	  const lines = await this.wrapText(ctx, name, 1160);
-	  ctx.fillText(lines.join('\n'), 147,330);//comment
+	  ctx.fillText(lines.join('\n'), 144,330);//comment
 	  ctx.beginPath();
  
  
-  ctx.drawImage(baseAvt1, 57, 295, 65, 69);
+  ctx.drawImage(baseAvt1, 57, 299, 63, 68);
   
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
