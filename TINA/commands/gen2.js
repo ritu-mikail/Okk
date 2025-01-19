be2ecbfc9cfdda5a8a54dce4fe03c482d7c8ef03
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-19 08:16:51 +0600

    Create gen2.js

---
+++ TINA/commands/gen2.js
@@ -0,0 +1,28 @@
+module.exports.config = {
+  name: "gen2",
+  version: "1.0.",
+  hasPermssion: 2,
+  credits: "nazrul",
+  description: "generate image from polination",
+  usePrefix: false,
+  commandCategory: "image",
+  usages: "query",
+  cooldowns: 2,
+};
+
+module.exports.run = async ({api, event, args }) => {
+const axios = require('axios');
+const fs = require('fs-extra');
+ let { threadID, messageID } = event;
+  let query = args.join(" ");
+  if (!query) return api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐏𝐫𝐨𝐯𝐢𝐝𝐞 𝐚 𝐏𝐫𝐨𝐦𝐩𝐭 𝐅𝐨𝐫 𝐓𝐡𝐞 𝐢𝐦𝐚𝐠𝐞....", threadID, messageID);
+  api.sendMessage("𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 𝐁𝐚𝐛𝐲...😘",event.threadID, event.messageID);
+let path = __dirname + `/cache/poli.png`;
+  const poli = (await axios.get(`https://sandipbaruwal.onrender.com/gen.html?prompt/${query}`, {
+    responseType: "arraybuffer",
+  })).data;
+  fs.writeFileSync(path, Buffer.from(poli, "utf-8"));
+  api.sendMessage({
+    body: `𝐈𝐦𝐚𝐠𝐞 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐟𝐮𝐥😘😘`,
+    attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID);
+};
