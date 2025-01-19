e667066aae4026284229110cda64f5c1aab06ba5
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-19 14:28:26 +0600

    Update aaaa.slap.js

--- TINA/commands/aaaa.slap.js
+++ TINA/commands/aaaa.slap.js
@@ -18,6 +18,6 @@ module.exports.run = async({ api, event, Threads, global }) => {
    var mention = Object.keys(event.mentions);
      let tag = event.mentions[mention].replace("@", "");
     if (!mention) return api.sendMessage("Mention 1 person that you want to slap", threadID, messageID);
-   var callback = () => api.sendMessage({body:`Slapped! ${tag}` + `\n\n*sorry, i thought there's mosquito in ur ugly face*`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slap.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slap.gif"));  
+   var callback = () => api.sendMessage({body:`Slapped! ${tag}` + `\n\n*আমার সঙ্গে লাগতে আসিস🐸🐸*`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slap.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slap.gif"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/slap.gif")).on("close",() => callback());
 }
