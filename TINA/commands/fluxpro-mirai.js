1a90b95ed1a9c9c07ebc90640f1f14c7ad143bd8
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-24 11:50:38 +0600

    Update fluxpro-mirai.js

--- TINA/commands/fluxpro-mirai.js
+++ TINA/commands/fluxpro-mirai.js
@@ -18,7 +18,7 @@ module.exports.config = {
   cooldowns: 15,
 };
 
-module.exports.run = async ({ event, args, api }) => {
+run = async ({ event, args, api }) => {
   try {
   const prompt = args.join(" ");
   /*let prompt2, ratio;
