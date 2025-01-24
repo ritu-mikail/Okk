109bea27914b13e6b7015f2bd3794245f1455ada
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-24 07:20:19 +0600

    Create fx4.js

---
+++ TINA/commands/fx4.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+module.exports.config = {
+    name: "fx4",
+    description: "Generate image from fal ai flux pro",
+    cooldown: 5,
+    commandCategory: "𝚗𝚘 𝚙𝚛𝚎𝚏𝚒𝚡",
+    hasPermssion: 0,
+    hasPrefix: false,
+    cooldowns: 5,
+    credits: "nazrul", //api by samir
+    usage: "{p}{n} ",
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        const { messageID } = event;
+        const prompt = args.join(' ');
+
+        if (!prompt) {
+            return api.sendMessage('Please provide a prompt', event.threadID, messageID);
+        }
+
+        const cliff = await new Promise(resolve => { 
+            api.sendMessage(`֎ 𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝗂𝗇𝗀 ${prompt} 𝗂𝗆𝖺𝗀𝖾...`, event.threadID, (err, info1) => {
+                resolve(info1);
+            }, event.messageID);
+        });
+
+        const apiUrl = `https://ashbina.onrender.com/gen2?prompt=${encodeURIComponent(prompt)}`;
+
+        const h = await axios.get(apiUrl, { responseType: 'stream' });
+
+        const message = {
+            attachment: h.data,   
+        };
+
+        api.unsendMessage(cliff.messageID);
+
+        api.sendMessage(message, event.threadID, event.messageID);
+    } catch (error) {
+        api.sendMessage(`An error occurred: ${error.message}`, event.threadID);
+    }
+};
