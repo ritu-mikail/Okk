0686aa7f78c5ed49e72476f8779636db592e0889
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-26 23:34:41 +0600

    Create fx7.js

---
+++ TINA/commands/fx7.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+module.exports.config = {
+    name: "fx7",
+    description: "Generate image from fal ai flux pro",
+    cooldown: 5,
+    commandCategory: "image",
+    hasPermssion: 0,
+    hasPrefix: false,
+    cooldowns: 5,
+    credits: "nazrul",
+    usage: "{p}{n} ",
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        const { messageID } = event;
+        const prompt = args.join(' ');
+
+        if (!prompt) {
+            return api.sendMessage('Please provide a prompt', event.threadID, messageID);
+        }
+
+        const nazrul = await new Promise(resolve => { 
+            api.sendMessage(`֎ 𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝗂𝗇𝗀 ${prompt} 𝗂𝗆𝖺𝗀𝖾...`, event.threadID, (err, info1) => {
+                resolve(info1);
+            }, event.messageID);
+        });
+
+        const apiUrl = `https://ts-ai-api-shuddho.onrender.com/api/animagine?prompt=${encodeURIComponent(prompt)}`;
+
+        const nazrulb = await axios.get(apiUrl, { responseType: 'stream' });
+
+        const message = {
+            attachment: nazrulb.data,   
+        };
+
+        api.unsendMessage(nazrul.messageID);
+
+        api.sendMessage(message, event.threadID, event.messageID);
+    } catch (error) {
+        api.sendMessage(`An error occurred: ${error.message}`, event.threadID);
+    }
+};
