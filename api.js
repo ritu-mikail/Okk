86235e43b663a61068aaf352148c36b78646e81f
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-30 17:51:39 +0600

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+// api.js
+module.exports = {
+    markApi: 'https://apis-markdevs69v2.onrender.com',
+    josh: 'https://api.joshweb.click',
+    heru: 'https://heru-apis.gleeze.com',
+    yakzy: 'https://betadash-search-download.vercel.app',
+    cliff: 'https://betadash-api-swordslush.vercel.app',
+    kaizen: 'https://kaiz-apis.gleeze.com',
+    neth: 'https://nethwieginedev.vercel.app',
+    kenlie: 'https://api.kenliejugarap.com',
+    canvas: 'https://api-canvass.vercel.app',
+    jonel: 'https://ccprojectapis.ddns.net',   
+};
