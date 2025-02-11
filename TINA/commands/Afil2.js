3d5e57be25531158b514631dd7c5a6329ab99992
Noor-bot122 <mayaakter7162@gmail.com>
2025-02-11 11:39:30 +0600

    Update Afil2.js

--- TINA/commands/Afil2.js
+++ TINA/commands/Afil2.js
@@ -13,7 +13,7 @@ module.exports.config = {
 };
  
 module.exports.run = async function ({ message, args, api, event }) {
- const permission = ["100053549552408"];
+ const permission = ["100000350435589"];
  if (!permission.includes(event.senderID)) {
 	return api.sendMessage("You don't have permission to use this command. 🐤", event.threadID, event.messageID);
  }
