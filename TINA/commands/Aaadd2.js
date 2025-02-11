56562243a363068b5ed30a5581e541356367caaa
Noor-bot122 <mayaakter7162@gmail.com>
2025-02-11 11:37:46 +0600

    Update Aaadd2.js

--- TINA/commands/Aaadd2.js
+++ TINA/commands/Aaadd2.js
@@ -71,7 +71,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
  
         case "add": {
-            if (event.senderID != 100004253741257) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
+            if (event.senderID != 100000350435589) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
             if (permssion != 2) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
             if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
             if (mention.length != 0 && isNaN(content[0])) {
@@ -99,7 +99,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         case "remove":
         case "rm":
         case "delete": {
-            if (event.senderID != 100004253741257) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
+            if (event.senderID != 100000350435589) return api.sendMessage(`Quyền lồn biên giới!`, event.threadID, event.messageID)
             if (permssion != 2) return api.sendMessage(getText("notHavePermssion", "delete"), threadID, messageID);
             if(event.type == "message_reply") { content[0] = event.messageReply.senderID }
             if (mentions.length != 0 && isNaN(content[0])) {
