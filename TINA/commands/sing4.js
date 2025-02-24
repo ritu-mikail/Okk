57369afdb9205dc2053728b6fc3bd4dc0c91d41f
NooBboY420 <167776712+NooBboY420@users.noreply.github.com>
2025-02-24 17:28:53 +0600

    Update and rename sing2.js to sing4.js

--- TINA/commands/sing4.js
+++ TINA/commands/sing4.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports.config = {
-    name: "sing",
+    name: "sing4",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "ryuko",
