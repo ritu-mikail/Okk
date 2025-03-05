dedd3bd2601a26678a883f910a370b267063616f
nazrul6x <mdrahulislam5767@gmail.com>
2025-03-05 20:39:40 +0600

    Update delete.js

--- TINA/commands/delete.js
+++ TINA/commands/delete.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "delete",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "nazrul",
 	description: "Delete 🖱️",
 	commandCategory: "Image",
 	usages: "rank",
