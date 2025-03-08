4fab7b56fe47f7e4a265b653e85864d883fc42e0
ritu-mikail <mdmikail673@gmail.com>
2025-03-08 07:43:57 +0600

    Update uid.js

--- TINA/commands/uid.js
+++ TINA/commands/uid.js
@@ -1,17 +1,26 @@
 module.exports.config = {
-	name: "uid",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "nazrul",
-  description: "get user id.",
-	commandCategory: "without prefix",
-	cooldowns: 5
+    "name": "link",
+    "version": "1.0.0",
+    "hasPermssion": 0,
+    "credits": "Nazrul",
+    "description": "get user id",
+    "commandCategory": "get user id",
+    "cooldowns": 0
 };
 
-module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
+module.exports.run = async function({ api, event, args }) {
+    const axios = global.nodemodule['axios']; 
+    if(event.type == "message_reply") { 
+	uid = event.messageReply.senderID
+	return api.sendMessage(`${uid}`, event.threadID, event.messageID) }
+    if (!args[0]) {return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);}
+    else {
+	if (args[0].indexOf(".com/")!==-1) {
+    const res_ID = await api.getUID(args[0]);  
+    return api.sendMessage(`${res_ID}`, event.threadID, event.messageID) }
 	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
+		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.keys(event.mentions)[i]}`, event.threadID);
 		return;
 	}
+}
 }
