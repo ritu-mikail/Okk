a136fe64355232d059c171e990d8cfbb7140ddc7
ritu-mikail <mdmikail673@gmail.com>
2025-06-01 15:27:35 +0600

    Update handleUnsend.js

--- includes/handle/handleUnsend.js
+++ includes/handle/handleUnsend.js
@@ -2,6 +2,7 @@ module.exports = function({ api }) {
    const reaction = "🥰";
      return function({ event }) {
    const { senderID, reaction, messageID } = event;
+   if (permssion != 2) return api.sendMessage("", messageID);
    if (senderID == api.getCurrentUserID()) {
    if (reaction == "🥰") return api.unsendMessage(messageID);
    }
