73fd13450c89352753e722edba0cac860eb40455
ritu-mikail <mdmikail673@gmail.com>
2025-06-01 15:46:52 +0600

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -295,7 +295,7 @@ module.exports = function({ api, models }) {
         handleEvent({ event });
         break;
       case "message_reaction":
-        if(event.senderID == api.getCurrentUserID() && event.reaction == '🥰') {
+        if(event.senderID == api.getCurrentUserID() && event.reaction == '🥀') {
 					api.unsendMessage(event.messageID)
 				}
         handleReaction({ event });
